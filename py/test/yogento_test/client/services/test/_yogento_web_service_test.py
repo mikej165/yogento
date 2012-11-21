ab3892b6cdd811f489dfb969ef497b9fdbce2097
minorg <github@minorgordon.net>
2012-11-20 21:46:31 -0500

    py: fixed _yogento_web_service_test URL

--- py/test/yogento_test/client/services/test/_yogento_web_service_test.py
+++ py/test/yogento_test/client/services/test/_yogento_web_service_test.py
@@ -1,8 +1,9 @@
-from yogento.client.services.user.impl.yogento_rest_user_service import \
-    YogentoRestUserService
+from yogento.client.services.user.impl.yogento_jsonrpc_user_service import \
+    YogentoJsonrpcUserService
 import logging
 
 
+logging.basicConfig(level=logging.DEBUG)
 class _YogentoWebServiceTest(object):
     API_PASSWORD = '_yogento_web_service_test'
     API_NETINFO = None
@@ -10,12 +11,12 @@ class _YogentoWebServiceTest(object):
     API_USERNAME = '_yogento_web_service_test'
     API_URL = None
     for api_netinfo, api_path in (
-        ('localhost:8080', '/yogento-java-server/api/'),
+        ('localhost:8080', '/yogento/api/'),
         ('yogento.com', '/api/'),
     ):
         api_url = "http://%(API_USERNAME)s:%(API_PASSWORD)s@%(api_netinfo)s%(api_path)s" % locals()
         try:
-            YogentoRestUserService(api_url).get_current_user()
+            YogentoJsonrpcUserService(api_url).get_current_user()
         except:
             logging.error("unable to contact %s", api_netinfo, exc_info=True)
             continue
