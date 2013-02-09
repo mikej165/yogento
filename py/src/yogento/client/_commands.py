914c4345edf037436df2c7122a27f9dda7df6b49
minorg <github@minorgordon.net>
2013-02-09 16:39:18 -0500

    py: eliminating yogento.client.dependencies

--- py/src/yogento/client/_commands.py
+++ py/src/yogento/client/_commands.py
@@ -1,19 +1,19 @@
 #-------------------------------------------------------------------------------
 # Copyright (c) 2013, Minor Gordon
 # All rights reserved.
-# 
+#
 # Redistribution and use in source and binary forms, with or without
 # modification, are permitted provided that the following conditions
 # are met:
-# 
+#
 #     * Redistributions of source code must retain the above copyright
 #       notice, this list of conditions and the following disclaimer.
-# 
+#
 #     * Redistributions in binary form must reproduce the above copyright
 #       notice, this list of conditions and the following disclaimer in
 #       the documentation and/or other materials provided with the
 #       distribution.
-# 
+#
 # THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND
 # CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES,
 # INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
@@ -30,8 +30,8 @@
 # OF SUCH DAMAGE.
 #-------------------------------------------------------------------------------
 
+from collections import OrderedDict
 from yogento.client._command import _Command
-from yogento.client.dependencies.ordereddict import OrderedDict
 
 
 class _Commands(OrderedDict):
--- py/src/yogento/client/_main.py
+++ py/src/yogento/client/_main.py
@@ -1,19 +1,19 @@
 #-------------------------------------------------------------------------------
 # Copyright (c) 2013, Minor Gordon
 # All rights reserved.
-# 
+#
 # Redistribution and use in source and binary forms, with or without
 # modification, are permitted provided that the following conditions
 # are met:
-# 
+#
 #     * Redistributions of source code must retain the above copyright
 #       notice, this list of conditions and the following disclaimer.
-# 
+#
 #     * Redistributions in binary form must reproduce the above copyright
 #       notice, this list of conditions and the following disclaimer in
 #       the documentation and/or other materials provided with the
 #       distribution.
-# 
+#
 # THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND
 # CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES,
 # INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
@@ -30,7 +30,7 @@
 # OF SUCH DAMAGE.
 #-------------------------------------------------------------------------------
 
-from yogento.client.dependencies.argparse import ArgumentParser
+from argparse import ArgumentParser
 from yogento.client.utils import class_qname
 import logging
 import sys
@@ -80,10 +80,10 @@ class _Main(object):
         else:
             logging_level = logging.WARNING
         logging.basicConfig(
-            format='%(asctime)s:%(module)s:%(lineno)s:%(name)s:%(levelname)s: %(message)s', #@IgnorePep8
+            format='%(asctime)s:%(module)s:%(lineno)s:%(name)s:%(levelname)s: %(message)s',  # @IgnorePep8
             level=logging_level
         )
-        #delattr(args, 'debug') # Pass debug= through to the command
+        # delattr(args, 'debug') # Pass debug= through to the command
         delattr(args, 'logging')
 
         command = commands[args.command]
